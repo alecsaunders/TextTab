cdcd054026cd068b2c77d9ad09699763b83786a1
Alec Saunders <alecsaunders90@gmail.com>
2018-01-01 11:33:17 -0700

    added attributes to setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,27 @@ def setup():
     verstion="0.1",
     description='Python package for reading and renduring TextTab (.txtab) files.',
     long_description='Python package for reading and renduring TextTab (.txtab) files.',
-    
+    url='https://github.com/uvu2112/TextTab',
+    author='Alec Saunders',
+    author_email='alecsaunders90@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='TextTab txtab guitar tab tablature',
+
+    python_requires='>=2.6, <4',
