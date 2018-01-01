e4c8fb88e7fe237ad25954562a1b809333c64478
Alec Saunders <alecsaunders90@gmail.com>
2017-12-31 20:32:30 -0700

    edited the setup.py file

---
+++ setup.py
@@ -0,0 +1,6 @@
+def setup():
+    name="TextTab",
+    verstion="0.1",
+    description='Python package for reading and renduring TextTab (.txtab) files.',
+    long_description='Python package for reading and renduring TextTab (.txtab) files.',
+    
